c6ac366900ec5fa08e13a180cd5ca1a082e8a121
Anton Sidelnikov <ndmeredian@gmail.com>
2014-06-17 13:26:44 +0700

    Add MemberOfRestriction rich samples

---
+++ test/samples/restrictions/member_or_restriction.sample.js
@@ -0,0 +1,29 @@
+var helper = require('./../../helper.js');
+var DSL = require('./../../../lib/dsl');
+
+describe('MemberOfRestriction', function() {
+    it('validates property names are properties of referenced object', function() {
+        var dsl = new DSL(function() {
+            this.key('cost', {type: 'object'}, function() {
+                 this.each_key(this.member_of('data.resources'), {type: 'number'});
+            });
+
+            this.key('data', {type: 'object'}, function() {
+                this.key('resources', {type: 'object'}, function() {
+                    this.each_key(this.any(), {type: 'string'});
+                });
+            });
+            
+        });
+
+        dsl.validate({
+            cost: { res_1: 100 },
+            data: { resources: { res_1: "Res_1 name", res_2: "Res_2 name"} }
+        }).should.be.true;
+
+        dsl.validate({
+            cost: { res_3: 100 },
+            data: { resources: { res_1: "Res_1 name", res_2: "Res_2 name"} }
+        }).should.be.false;
+    });
+});
