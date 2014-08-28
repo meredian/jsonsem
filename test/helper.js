ad582f6d826e1a6f2615c62ef1e613e09266d9d5
Anton Sidelnikov <ndmeredian@gmail.com>
2014-08-28 21:37:07 +0700

    Add helper require method

--- test/helper.js
+++ test/helper.js
@@ -1,8 +1,13 @@
+var path = require('path');
 var chai = require('chai');
 chai.should();
 chai.use(require('sinon-chai'));
 var sinon = exports.sinon = require('sinon');
 var sandbox = exports.sandbox = null;
+
+exports.require = function(lib_path) {
+    return require(path.join(__dirname, '../lib', lib_path));
+};
 
 beforeEach(function() {
     exports.sandbox = sinon.sandbox.create();
