5539648170df6305fed41bfc679d7e2453900ea3
Anton Sidelnikov <ndmeredian@gmail.com>
2014-11-22 02:39:09 +0600

    Extend error reporting

--- test/samples/extension.sample.js
+++ test/samples/extension.sample.js
@@ -17,14 +17,17 @@ describe('Type inheritance and overriding', function() {
         });
 
         this.key('type_2', {type: 'type_3'}, function() {
-            this.extend('base', {}, function() {
-                this.property('name', function(value) {
-                    return true;
+            this.extend('number', {}, function() {
+                this.property('min', function(min, value) {
+                    return min <= value;
+                });
+                this.property('max', function(max, value) {
+                    return max >= value;
                 });
                 this.method('hohoho', function(txt) { console.log(txt); });
             });
             this.ololo("OVERRIDED");
-            this.key('int', {type: 'int', name: "aaa"}, function() {
+            this.key('int', {type: 'int', min: 2, max: 12}, function() {
                 this.hohoho("EXTENDED BASE CLASS!");
             });
         });
